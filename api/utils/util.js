8962016100411dd6d4eb1410c2c9df97b4619997
TrasherDK <trasherdk@fumlersoft.dk>
2021-05-26 13:16:40 +0200

    fix: stuff eslint bitch about

--- api/utils/util.js
+++ api/utils/util.js
@@ -1,18 +1,19 @@
-const figlet = require('figlet');
+/* eslint-disable camelcase */
+const figlet = require('figlet')
 const logger = require('../configs/logger')
 
 class Utils {
-    static show_banner() {
-        logger.info('(Utils) call show_banner()')
-        return new Promise((resolve, reject) => {
-            figlet('KCYBER TOPUP API', function(err, data) {
-                if (err) {
-                    reject('Something went wrong...')
-                }
-                resolve(data)
-            });
-        });
-    }
+  static show_banner () {
+    logger.info('(Utils) call show_banner()')
+    return new Promise((resolve, reject) => {
+      figlet('KCYBER TOPUP API', function (err, data) {
+        if (err) {
+          reject(new Error('Something went wrong...'))
+        }
+        resolve(data)
+      })
+    })
+  }
 }
 
-module.exports = Utils;
+module.exports = Utils
